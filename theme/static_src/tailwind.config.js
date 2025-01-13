e05c4c122ab5713713b500babd9cf52f78e15945
cangeorgecode <root@MSI>
2025-01-13 18:47:50 +0800

    init

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: [
+    "../templates/**/*.html",
+    "../../templates/**/*.html",
+    "../../**/templates/**/*.html",
+  ],
+  theme: {
+    extend: {
+      padding: {
+        4: "1rem",
+        5: "1.25rem",
+        6: "1.5rem",
+        7: "1.75rem",
+        8: "2rem",
+        9: "2.25rem",
+        10: "2.5rem",
+        11: "2.75rem",
+        12: "3rem",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("daisyui"),
+  ],
+};
